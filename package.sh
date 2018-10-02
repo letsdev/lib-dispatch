3e735a7b82384b124686b9fe9869b141d7827771
Martin Schwamberger <martin.schwamberger@letsdev.de>
2018-10-02 08:50:25 +0200

    Add travis file

--- package.sh
+++ package.sh
@@ -7,20 +7,10 @@ if [ ! -d "${LIBS_HOME}" ]; then
     exit 1
 fi
 
-MVN_VERSION=$(mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    exec:exec)
-
-echo "EXPORT TRAVIS_TAG to ${MVN_VERSION}"
-export TRAVIS_TAG=${MVN_VERSION}
-
 cd ${LIBS_HOME}/android
-zip -r dispatch-${MVN_VERSION}-android.zip .
+zip -r dispatch-${TRAVIS_TAG}-android.zip .
+mv *.zip ${LIBS_HOME}
 
 cd ${LIBS_HOME}/linux
-zip -r dispatch-${MVN_VERSION}-linux.zip .
-
-echo "Add tag ${TRAVIS_TAG}"
-git tag "${TRAVIS_TAG}" || true
+zip -r dispatch-${TRAVIS_TAG}-linux.zip .
+mv *.zip ${LIBS_HOME}
