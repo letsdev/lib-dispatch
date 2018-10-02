f9c86176d6150a8fb5687728a9abb804b327dab6
Martin Schwamberger <martin.schwamberger@letsdev.de>
2018-10-02 14:35:51 +0200

    Fix package and travis release files

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 LIBS_HOME=/home/build/app/libs
+TARGET_DIR=/home/build/app/dist
 
 if [ ! -d "${LIBS_HOME}" ]; then
     echo "Missing library folder"    
@@ -9,8 +10,8 @@ fi
 
 cd ${LIBS_HOME}/android
 zip -r dispatch-${TRAVIS_TAG}-android.zip .
-mv *.zip ${LIBS_HOME}
+mv *.zip ${TARGET_DIR}
 
 cd ${LIBS_HOME}/linux
 zip -r dispatch-${TRAVIS_TAG}-linux.zip .
-mv *.zip ${LIBS_HOME}
+mv *.zip ${TARGET_DIR}
